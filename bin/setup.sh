fcf9afea778052dae82201d9d580acfb15117cff
David L Whitehurst <me@dlwhitehurst.com>
2023-11-22 18:33:31 -0500

    initial commit

--- bin/setup.sh
+++ bin/setup.sh
@@ -14,22 +14,17 @@ Usage:
 
 Options:
     --shell     Initialize shell environment variables (bash only)
-    --compose   Get docker-compose and setup in isolation
     --utils     Clone the VirtualYou utilities
-    --nodejs    Install NVM, nodejs and set version based on vyrc
-    --curl      Initialize .curlrc, (mTLS settings)
+    --compose   Get docker-compose and setup in isolation
     --apis      Clone the api repos
     --app       Clone the current UI application
-    --api-pkg   Clone, build, and package
-    --ui-pkg    Clone, build, and package
-    --pushall   Push all tagged images
 ENDHELP
     exit
 fi
 ################################################################################
 
 BASH_DIR="$( cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd )"
-VY_PROJECTS=$( cd ${BASH_DIR}/../../virtualyou && pwd )
+VY_PROJECTS=$( cd ${BASH_DIR}/../.. && pwd )
 
 # ignore any command customizations
 unalias cat  2> /dev/null
@@ -47,8 +42,6 @@ while [[ $# > 0 && "${1}" =~ ^-- ]]; do
         --shell)    initShell=true;     noAction=false; shift 1 ;;
         --utils)    cloneUtils=true;     noAction=false; shift 1 ;;
         --compose)  initCompose=true;   noAction=false; shift 1 ;;
-        --nodejs)   initNodeJS=true;    noAction=false; shift 1 ;;
-        --curl)     initCurl=true;      noAction=false; shift 1 ;;
         --apis)     cloneApis=true;     noAction=false; shift 1 ;;
         --app)      cloneApp=true;      noAction=false; shift 1 ;;
             *) echo "Unrecognized option: ${1}" >&2; exit 1 ;;
@@ -62,11 +55,12 @@ fi
 
 # --------------------------------------------------
 # shell setup
+echo $VY_PROJECTS
 
 if [[ ${initShell} == true ]]; then
     if [[ -e ~/.bashrc ]]; then
         echo "Adding vytools/vyrc to ~/.bashrc"
-        sed_i -e '/vyrc/d' ~/.bashrc
+        sed -i '/vyrc/d' ~/.bashrc
         echo "source ${VY_PROJECTS}/vytools/vyrc" >> ~/.bashrc
     fi
 fi
@@ -85,37 +79,10 @@ if [[ ${initCompose} == true ]]; then
 fi
 
 # --------------------------------------------------
-# NodeJS setup
-
-if [[ ${initNodeJS} == true ]]; then
-    echo "This is pending implementation."
-fi
-
-# --------------------------------------------------
-# curl setup
-
-if [[ ${initCurl} == true ]]; then
-    if [[ -z ${VY_CONFIGS} ]]; then
-        echo "Cannot continue, VY_CONFIGS is not defined." >&2
-        exit 1
-    fi
-    source ${VY_CONFIGS}/.pkirc
-    echo Populating ~/.curlrc
-    cat > ~/.curlrc <<EOF
-# mTLS keystores
---cacert ${VY_CONFIGS}/pki/cdx_cacerts.pem
---cert   ${VY_CONFIGS}/pki/cdx_keystore.pem:${CDX_KEYSTORE_PASS}
-EOF
-fi
-
-# --------------------------------------------------
 # repo cloning
 
 if [[ ${cloneUtils} == true ]]; then
-    if [[ -z ${NODE_VERSION} ]]; then
-        echo "Cannot continue, NODE_VERSION is not defined." >&2
-        exit 1
-    fi
+    cd $VY_PROJECTS
     ( set -ex
       git clone -b main     git@github.com:dlwhitehurst/vymain.git
       git clone -b main     git@github.com:dlwhitehurst/vyconfigs.git
@@ -124,10 +91,7 @@ if [[ ${cloneUtils} == true ]]; then
 fi
 
 if [[ ${cloneApis} == true ]]; then
-    if [[ -z ${NODE_VERSION} ]]; then
-        echo "Cannot continue, NODE_VERSION is not defined." >&2
-        exit 1
-    fi
+    cd $VY_PROJECTS
     ( set -ex
       git clone -b main     git@github.com:dlwhitehurst/userauth.git
       git clone -b main     git@github.com:dlwhitehurst/personal.git
@@ -138,11 +102,9 @@ if [[ ${cloneApis} == true ]]; then
     )
 fi
 if [[ ${cloneApp} == true ]]; then
-    if [[ -z ${NODE_VERSION} ]]; then
-        echo "Cannot continue, NODE_VERSION is not defined." >&2
-        exit 1
-    fi
+    cd $VY_PROJECTS
     ( set -ex
       git clone -b main git@github.com:dlwhitehurst/vite-mvp.git
     )
 fi
+
---
+++ bin/tool.sh
@@ -0,0 +1,124 @@
+#!/bin/bash
+# A VirtualYou project tool utility
+
+################################################################################
+if [[ "$1" == "--help" ]]; then
+    cat <<'ENDHELP'
+Welcome to the VirtualYou Tool utility.
+
+Usage:
+  tool.sh [options]
+
+
+Options:
+    --dclean    Clean all containers then all images (w/prompts)
+    --pkg       Build and package APIs
+    --app-pkg   Build and package UI application
+    --test      Test script sandbox
+    --ui-pkg    Build and package
+    --push-all  Push all tagged images
+ENDHELP
+    exit
+fi
+################################################################################
+
+BASH_DIR="$( cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd )"
+VY_PROJECTS=$( cd ${BASH_DIR}/../.. && pwd )
+
+# Get the version number from the first positional parameter
+BUILD_VERSION=$2
+
+
+# ignore any command customizations
+unalias cat  2> /dev/null
+unalias cp   2> /dev/null
+unalias git  2> /dev/null
+unalias sudo 2> /dev/null
+
+noAction=true
+
+# --------------------------------------------------
+# parse command line options
+
+while [[ $# > 0 && "${1}" =~ ^-- ]]; do
+    case "${1}" in
+        --dclean)   dockerClean=true;   noAction=false; shift 1 ;;
+        --pkg)      packageApis=true;      noAction=false; shift 1 ;;
+        --app-pkg)  packageApp=true;   noAction=false; shift 1 ;;
+        --test)     testScript=true;    noAction=false; shift 1 ;;
+        *) echo "Unrecognized option:         ${1}" >&2; exit 1 ;;
+    esac
+done
+
+if [[ ${noAction} == true ]]; then
+    echo "No action was specified" >&2
+    exit 1
+fi
+
+# --------------------------------------------------
+# docker clean
+
+if [[ ${dockerClean} == true ]]; then
+  tmp=$(docker ps)
+  echo "$tmp"
+
+  if [[ ${tmp} == 'CONTAINER ID   IMAGE     COMMAND   CREATED   STATUS    PORTS     NAMES' ]]; then
+    # Prompt the user for confirmation
+    read -p "Do you want to proceed? (y/n) " choice
+
+    # Check the user's response
+    case "$choice" in
+      y|Y ) echo "Deleting all containers ...";;
+      n|N ) echo "Exiting..."; exit;;
+      * ) echo "Invalid response"; exit;;
+    esac
+
+    # Delete all Docker Containers
+    ( set -ex
+      docker rm -f $(docker ps -aq)
+    )
+
+    # Delete all Docker Images
+    echo "Now, deleting all images"
+    ( set -ex
+      docker rmi -f $(docker images -aq)
+    )
+  else
+    echo "You need to stop running Docker containers first.";
+    exit;
+  fi
+fi
+
+# --------------------------------------------------
+# API packages
+if [[ ${packageApis} == true ]]; then
+
+#  BUILD_VERSION=0.1.0
+
+  cd $VY_PROJECTS
+  ( set -ex
+    cd userauth
+    docker build -t dlwhitehurst/userauth:$BUILD_VERSION .
+    cd ../personal
+    docker build -t dlwhitehurst/personal:$BUILD_VERSION .
+    cd ../medical
+    docker build -t dlwhitehurst/medical:$BUILD_VERSION .
+    cd ../financial
+    docker build -t dlwhitehurst/financial:$BUILD_VERSION .
+    cd ../administration
+    docker build -t dlwhitehurst/administration:$BUILD_VERSION .
+  )
+fi
+
+# --------------------------------------------------
+# Test Script
+if [[ ${testScript} == true ]]; then
+  echo $BUILD_VERSION
+fi
+
+# --------------------------------------------------
+# UI package
+if [[ ${packageApp} == true ]]; then
+  echo $BUILD_VERSION
+  echo "This is under construction."
+fi
