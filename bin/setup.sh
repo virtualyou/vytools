ac0daba771e64880ddffe8621aae850ab002ae6a
David L Whitehurst <me@dlwhitehurst.com>
2023-11-22 15:54:18 -0500

    initial commit

--- bin/setup.sh
+++ bin/setup.sh
@@ -13,8 +13,9 @@ Usage:
 
 
 Options:
-    --shell     Initialize shell environment variables (bash and/or zsh)
-                (May prompt for sudo password while updating /etc/profile.d)
+    --shell     Initialize shell environment variables (bash only)
+    --compose   Get docker-compose and setup in isolation
+    --utils     Clone the VirtualYou utilities
     --nodejs    Install NVM, nodejs and set version based on vyrc
     --curl      Initialize .curlrc, (mTLS settings)
     --apis      Clone the api repos
@@ -28,8 +29,7 @@ fi
 ################################################################################
 
 BASH_DIR="$( cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-VY_PROJECTS=$( cd ${BASH_DIR}/../.. && pwd )
+VY_PROJECTS=$( cd ${BASH_DIR}/../../virtualyou && pwd )
 
 # ignore any command customizations
 unalias cat  2> /dev/null
@@ -44,12 +44,14 @@ noAction=true
 
 while [[ $# > 0 && "${1}" =~ ^-- ]]; do
     case "${1}" in 
-        --shell) initShell=true;     noAction=false; shift 1 ;;
-         --nodejs) initNodeJS=true;      noAction=false; shift 1 ;;
-         --curl) initCurl=true;      noAction=false; shift 1 ;;
-        --apis)  cloneApis=true;     noAction=false; shift 1 ;;
-       --app)    cloneApp=true;      noAction=false; shift 1 ;;
-              *) echo "Unrecognized option: ${1}" >&2; exit 1 ;;
+        --shell)    initShell=true;     noAction=false; shift 1 ;;
+        --utils)    cloneUtils=true;     noAction=false; shift 1 ;;
+        --compose)  initCompose=true;   noAction=false; shift 1 ;;
+        --nodejs)   initNodeJS=true;    noAction=false; shift 1 ;;
+        --curl)     initCurl=true;      noAction=false; shift 1 ;;
+        --apis)     cloneApis=true;     noAction=false; shift 1 ;;
+        --app)      cloneApp=true;      noAction=false; shift 1 ;;
+            *) echo "Unrecognized option: ${1}" >&2; exit 1 ;;
     esac
 done
 
@@ -59,37 +61,27 @@ if [[ ${noAction} == true ]]; then
 fi
 
 # --------------------------------------------------
-# OS compatibility shimming
-
-SUDO=sudo
-sed_i() { sed -i "$@"; }
-
-if [[ "$(uname -s)" == 'CYGWIN'* ]]; then
-    SUDO=
-fi
-if [[ "$(uname -s)" == 'Darwin'* ]]; then
-    sed_i() { sed -i '' "$@"; }
-fi
-
-# --------------------------------------------------
 # shell setup
 
 if [[ ${initShell} == true ]]; then
-    if [[ -e ~/.zshrc ]]; then
-        echo "Adding vytools/vyrc to ~/.zshrc"
-        sed_i -e '/vyrc/d' ~/.zshrc
-        echo "source ${VY_PROJECTS}/vytools/vyrc" >> ~/.zshrc
-    fi
     if [[ -e ~/.bashrc ]]; then
         echo "Adding vytools/vyrc to ~/.bashrc"
         sed_i -e '/vyrc/d' ~/.bashrc
         echo "source ${VY_PROJECTS}/vytools/vyrc" >> ~/.bashrc
     fi
-    # copy-in user configs
-    if [[ ! -e ~/.exrc ]]; then
-        echo "Adding ~/.exrc (vi editor defaults)"
-        cp ${VY_PROJECTS}/vytools/host-configs/~/.exrc ~
-    fi
+fi
+
+# --------------------------------------------------
+# docker-compose setup
+
+if [[ ${initCompose} == true ]]; then
+  echo "BASH_DIR is $BASH_DIR"
+  echo "VY_PROJECTS is ${VY_PROJECTS}"
+
+  cd ${VY_PROJECTS}
+  mkdir docker
+  cd docker
+  cp ${VY_PROJECTS}/vymain/docker-compose.yaml .
 fi
 
 # --------------------------------------------------
@@ -119,6 +111,18 @@ fi
 # --------------------------------------------------
 # repo cloning
 
+if [[ ${cloneUtils} == true ]]; then
+    if [[ -z ${NODE_VERSION} ]]; then
+        echo "Cannot continue, NODE_VERSION is not defined." >&2
+        exit 1
+    fi
+    ( set -ex
+      git clone -b main     git@github.com:dlwhitehurst/vymain.git
+      git clone -b main     git@github.com:dlwhitehurst/vyconfigs.git
+      git clone -b main     git@github.com:dlwhitehurst/vydata.git
+    )
+fi
+
 if [[ ${cloneApis} == true ]]; then
     if [[ -z ${NODE_VERSION} ]]; then
         echo "Cannot continue, NODE_VERSION is not defined." >&2
@@ -128,6 +132,9 @@ if [[ ${cloneApis} == true ]]; then
       git clone -b main     git@github.com:dlwhitehurst/userauth.git
       git clone -b main     git@github.com:dlwhitehurst/personal.git
       git clone -b main     git@github.com:dlwhitehurst/medical.git
+      git clone -b main     git@github.com:dlwhitehurst/financial.git
+      git clone -b main     git@github.com:dlwhitehurst/administration.git
+
     )
 fi
 if [[ ${cloneApp} == true ]]; then
